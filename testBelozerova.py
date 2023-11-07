e47710d353df7b49fa7ec3a43227543b42dfa0f0
Liza-Belozerova <lisa_15011998@mail.ru>
2023-11-07 14:38:22 +0300

    Комментарии

--- testBelozerova.py
+++ testBelozerova.py
@@ -1,14 +1,17 @@
 # Елизавета Белозерова, 10-я когорта — Финальный проект. Инженер по тестированию плюс
 import request, data
 
+# сохранение трек номера
 def get_track():
     responce = request.post_new_order(data.body)
     return responce.json()["track"]
 
+# подставили трек номер в запрос на получение информации о заказе
 def get_order():
     responce = request.get_order_track(str(get_track()))
     return responce
 
+# тест на проверку приходит ли в ответе 200 код
 def test_status_code():
     responce = get_order()
     assert responce.status_code == 200
